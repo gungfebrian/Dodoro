9d70ef4de87a2f53e4ce954ac972defa717107f1
Alan Brian Frederick <alanbrianf25@gmail.com>
2026-04-21 14:16:46 +0800

    Initial Commit

---
+++ Challenge 2/View/ShopView.swift
@@ -0,0 +1,18 @@
+//
+//  ShopView.swift
+//  Challenge 2
+//
+//  Created by Alan Brian Frederick on 21/04/26.
+//
+
+import SwiftUI
+
+struct ShopView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShopView()
+}
