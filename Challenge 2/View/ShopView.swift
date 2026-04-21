f8a00a294137a00688ccf44f0a723ab47a7d9a72
Alanbrianf <alanbrianf25@gmail.com>
2026-04-21 14:22:05 +0800

    Update ShopView.swift

--- Challenge 2/View/ShopView.swift
+++ Challenge 2/View/ShopView.swift
@@ -8,8 +8,76 @@
 import SwiftUI
 
 struct ShopView: View {
+    
+    @State private var money: Int = 24
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Image("titikkertas")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            
+            VStack{
+                HStack {
+                    Text("Shop")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(.trailing, 140)
+                    
+                    
+                    HStack {
+                        Text("􁞱")
+                            .font(Font.system(size: 24))
+                            .foregroundStyle(Color.blue)
+                        
+                        Image(systemName: "multiply")
+                        
+                        Text(String(money))
+                            .font(.system(size: 24, weight: .bold))
+                    }.padding()
+                }
+                
+                HStack {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 30)
+                            .fill(Color.lightGray)
+                            .strokeBorder(Color.darkGray, lineWidth: 5)
+                            .frame(width: 170, height: 170)
+                        
+                        VStack {
+                            Image("Dodit")
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                        
+                            Text("Dodit")
+                                .font(.system(size: 21, design: .rounded))
+                                .bold()
+                        }
+                    }.padding(.trailing, 10)
+                    
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 30)
+                            .fill(Color.lightGray)
+                            .strokeBorder(Color.darkGray, lineWidth: 5)
+                            .frame(width: 170, height: 170)
+                        
+                        VStack {
+                            Image("Scribi")
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                        
+                            Text("Scribi")
+                                .font(.system(size: 21, design: .rounded))
+                                .bold()
+                        }
+                    }.padding(.trailing, 10)
+                }
+            }
+        }
+        
+        
+                
     }
 }
 
